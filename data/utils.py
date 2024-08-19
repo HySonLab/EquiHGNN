169b8968b92bf4fdfff057c9c7bf7ee129ea14f4
tien02 <tien.danganh02@gmail.com>
2024-08-20 00:28:54 +0700

    feat: rings as the hypergraphs

--- data/utils.py
+++ data/utils.py
@@ -5,6 +5,29 @@ from ogb.utils.features import atom_to_feature_vector, bond_to_feature_vector
 from rdkit import Chem
 
 
+def compute_ring_features(ring: frozenset[int], molecule: Chem.Mol) -> tuple[float]:
+    """ https://github.com/NSAPH-Projects/topological-equivariant-networks/blob/main/etnn/qm9/lifts/ring.py """
+    ring_atoms = [molecule.GetAtomWithIdx(idx) for idx in ring]
+    ring_size = float(len(ring))
+    is_aromatic = float(all(atom.GetIsAromatic() for atom in ring_atoms))
+    has_heteroatom = float(any(atom.GetSymbol() not in ("C", "H") for atom in ring_atoms))
+    is_saturated = float(
+        all(atom.GetHybridization() == Chem.HybridizationType.SP3 for atom in ring_atoms)
+    )
+    return (ring_size, is_aromatic, has_heteroatom, is_saturated)
+
+
+def extract_ring_info(mol) -> set[tuple[set, set]]:
+    """ Indentify rings in a graph """
+    cells = set()
+    ring_info = mol.GetRingInfo()
+    for ring in ring_info.AtomRings():
+        node_idc = frozenset(ring)
+        feature_vector = compute_ring_features(node_idc, mol)
+        cells.add((node_idc, feature_vector))
+    return cells
+
+
 def he_conj(mol):
     """ get node index and hyperedge index of conjugated structure in a molecule
 
@@ -66,53 +89,27 @@ def smi2hgraph(smiles_string):
         bond_fvs = np.empty((0, num_bond_features), dtype=np.int64)
         return (atom_fvs, n_idx, e_idx, bond_fvs)
     
-    # hyperedges for conjugated bonds
-    he_n, he_e = he_conj(mol)
+    # hyperedges for rings
+    cells = extract_ring_info(mol=mol)
+    he_n, he_e = [], []
+
+    for idx, (node_idc, fvs) in enumerate(cells):
+        for atom_idx in node_idc:
+            he_n.append(atom_idx)
+            he_e.append(idx)
+
+        # TODO: explore an ideal ring feature
+        # Use 'has_heteroatom' as feature
+        bond_fvs += [[int(fvs[-2])]]
+
     num_bond = mol.GetNumBonds()
     if len(he_n) != 0:
         he_e = [_id + num_bond for _id in he_e]
         n_idx += he_n
         e_idx += he_e
-        bond_fvs += len(set(he_e)) * [num_bond_features * [5]]
 
     return (atom_fvs, n_idx, e_idx, bond_fvs)
 
-def mol2hgraph(mol):
-    """
-    """
-    
-    # atoms
-    atom_fvs = []
-    for atom in mol.GetAtoms():
-        atom_fvs.append(atom_to_feature_vector(atom))
-
-    # bonds
-    num_bond_features = 1  # bond type (single, double, triple, conjugated)
-    if len(mol.GetBonds()) > 0: # mol has bonds
-        n_idx, e_idx, bond_fvs = [], [], []
-        for i, bond in enumerate(mol.GetBonds()):
-            n_idx.append(bond.GetBeginAtomIdx())
-            n_idx.append(bond.GetEndAtomIdx())
-            e_idx.append(i)
-            e_idx.append(i)
-            bond_type = bond_to_feature_vector(bond)[0]
-            bond_fvs.append([bond_type])
-
-    else:   # mol has no bonds
-        n_idx, e_idx= [], []
-        bond_fvs = np.empty((0, num_bond_features), dtype=np.int64)
-        return (atom_fvs, n_idx, e_idx, bond_fvs)
-    
-    # hyperedges for conjugated bonds
-    he_n, he_e = he_conj(mol)
-    num_bond = mol.GetNumBonds()
-    if len(he_n) != 0:
-        he_e = [_id + num_bond for _id in he_e]
-        n_idx += he_n
-        e_idx += he_e
-        bond_fvs += len(set(he_e)) * [num_bond_features * [5]]
-
-    return (atom_fvs, n_idx, e_idx, bond_fvs)
 
 class HData(Data):
     """ PyG data class for molecular hypergraphs

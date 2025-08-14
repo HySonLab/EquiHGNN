4a3a1f6e13fd217387b298047e2ca4e4a604d3f7
tien02 <tien.danganh02@gmail.com>
2025-08-14 13:35:37 +0700

    Add MD17 dataset

---
+++ scripts/run_md17.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+set -euxo pipefail
+
+# 0-aspirin, 1-benzene, 2-ethanol, 3-malonaldehyde,
+# 4-naphthalene, 5-salicylic acid, 6-toluene, 7-u0, 8-uracil,
+
+TASK=$1
+
+# Use default settings
+python -u main.py \
+    --method mhnnm \
+    --data_dir datasets/md17 \
+    --data md17_hg \
+    --target $TASK \
+    --output_num_layers 3 \
+    --MLP_hidden 256 \
+    --output_hidden 128 \
+    --aggregate mean \
+    --lr 0.0001 \
+    --clip_gnorm 5.0 \
+    --dropout 0.0 \
+    --batch_size 768 \
+    --epochs 400 \
